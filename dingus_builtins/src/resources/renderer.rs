e3808c9f0b6ca5ded6e9674e6fff5ded5a865439
anti-prism <nooby3.1415altalt@gmail.com>
2026-03-17 08:20:13 -0500

    better rendering

--- dingus_builtins/src/resources/renderer.rs
+++ dingus_builtins/src/resources/renderer.rs
@@ -1,5 +1,6 @@
-use minifb::{Window, WindowOptions, Key};
-use std::time::{Instant, Duration};
+use minifb::{Window, WindowOptions, Key, clamp};
+use crate::consts::{SCREEN_HEIGHT, SCREEN_WIDTH};
+
 #[derive(Debug)]
 pub struct Renderer {
     pub window: Window,
@@ -26,38 +27,51 @@ impl Renderer {
         Self { window, buffer, width, height }
     }
 
+    // todo: test optimized render methods
     pub fn clear(&mut self, color: u32) {
-        for pixel in self.buffer.iter_mut() {
-            *pixel = color;
+        self.buffer.fill(color);
+    }
+
+    pub fn draw_pixel(&mut self, x: i32, y: i32, color: u32) {
+        if Self::is_in_bounds(x, SCREEN_WIDTH as i32) && Self::is_in_bounds(y, SCREEN_HEIGHT as i32) {
+            self.buffer[y as usize * SCREEN_WIDTH + x as usize] = color;
         }
     }
 
-    pub fn draw_pixel(&mut self, x: usize, y: usize, color: u32) {
-        if x < self.width && y < self.height {
-            self.buffer[y * self.width + x] = color;
-        }
-    }
-
-    pub fn draw_rect(&mut self, x: usize, y: usize, w: usize, h: usize, color: u32) {
-        for dy in 0..h {
-            for dx in 0..w {
-                self.draw_pixel(x + dx, y + dy, color);
-            }
+    pub fn draw_rect(&mut self, x: i32, y: i32, w: i32, h: i32, color: u32) {
+        let left = Self::restrict_to_bounds(x, SCREEN_WIDTH as i32) as usize;
+        let right = Self::restrict_to_bounds(x + w, SCREEN_WIDTH as i32) as usize;
+        for i in y..(y + h) {
+            let offset = i as usize * SCREEN_WIDTH;
+            self.buffer[(offset + left)..(offset + right)].fill(color);
         }
     }
 
     pub fn draw_circle(&mut self, cx: i32, cy: i32, radius: i32, color: u32) {
-        for y in -radius..=radius {
-            for x in -radius..=radius {
-                if x*x + y*y <= radius*radius {
-                    let px = cx + x;
-                    let py = cy + y;
-                    if px >= 0 && py >= 0 {
-                        self.draw_pixel(px as usize, py as usize, color);
-                    }
-                }
+        let rad2 = radius * radius;
+        for y in 0..radius {
+            let x = (rad2 - y * y).isqrt();
+            let left = Self::restrict_to_bounds(cx - x, SCREEN_WIDTH as i32) as usize;
+            let right = Self::restrict_to_bounds(cx + x, SCREEN_WIDTH as i32) as usize;
+            let top = cy - y;
+            let bottom = cy + y;
+            if Self::is_in_bounds(top, SCREEN_HEIGHT as i32) {
+                self.buffer[(top as usize * SCREEN_WIDTH + left)..(top as usize * SCREEN_WIDTH + right)].fill(color);
+            }
+            if Self::is_in_bounds(bottom, SCREEN_HEIGHT as i32) {
+                self.buffer[(bottom as usize * SCREEN_WIDTH + left)..(bottom as usize * SCREEN_WIDTH + right)].fill(color);
             }
         }
+    }
+
+    fn restrict_to_bounds(x: i32, b: i32) -> i32 {
+        if x < 0 { 0 }
+        else if x >= b { b - 1 }
+        else { x }
+    }
+
+    fn is_in_bounds(x: i32, b: i32) -> bool {
+        x >= 0 && x < b
     }
 
     pub fn update(&mut self) {
